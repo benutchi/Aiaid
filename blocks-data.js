e4cfc2a653583175cd4c80525271c37ce64e845f
benutchi <benny.gustafsson@idrottsskolannacka.se>
2026-02-21 22:05:17 +0100

    Update blocks-data.js

--- blocks-data.js
+++ blocks-data.js
@@ -1,1 +1,8 @@
+// blocks-data.js
+// All data för Studiekompisen ligger här.
+// Index.html läser bara window.BLOCKS_DATA
 
+window.BLOCKS_DATA = [
+  // KLIS TRA IN HELA din const BLOCKS = [ ... ] här,
+  // MEN: ta bort "const BLOCKS =" och behåll bara arrayen.
+];
